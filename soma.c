803f244f75653d2d2468575fed39b4a0e7a67fbc
brayanhenrique <112041338+brayanhenrique@users.noreply.github.com>
2024-04-10 15:54:52 -0300

    Add files via upload

---
+++ soma.c
@@ -0,0 +1,20 @@
+void(){
+    int n1,n2,n3,n4,soma;
+    
+    printf("n1: ");
+    scanf("%d",&n1);
+    printf("n2: ");
+    scanf("%d",&n2);
+    prntf("n3: ");
+    scanf("%d",&n3);
+    printf("n4: ");
+    scanf("%d",&n4)
+
+    soma = (n1+n2+n3+n4);
+
+    printf("resultado da soma: %d",soma);
+
+
+
+
+}
